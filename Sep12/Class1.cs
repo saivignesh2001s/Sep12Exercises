77b3880a0b18ab70b7a75bc1fa3cd0947f5b3fb2
saivignesh2001s <saivignesh2001s@gmail.com>
2022-09-12 15:47:47 +0530

    Add project files.

---
+++ Sep12/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sep12
+{
+    public class Calculator
+    {
+        public double number
+        {
+            get;
+            set;
+        }
+        public double Cube1(double number)
+        {
+
+            double k = number * number * number;
+            return k;
+        }
+        public double squareroot(double number)
+        {
+            double ci=Math.Sqrt(number);
+            return ci;
+        }
+    }
+}
---
+++ Sep12Exercises/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+namespace Sep12Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\saivignesh\source\repos\Sep12Exercises\Sep12\bin\Debug\Sep12.dll";
+            Assembly asm=Assembly.LoadFrom(path);
+            Type[] types = asm.GetTypes();
+            Type t = null;
+            foreach (var item in types)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.FullName);
+                MemberInfo[] min = item.GetMembers();
+                foreach(var item1 in min)
+                {
+                    Console.WriteLine(item1.Name);
+                }
+                MethodInfo[] max = item.GetMethods();
+                foreach (var item2 in max)
+                {
+                    Console.WriteLine(item2.Name);
+
+                    ParameterInfo[] pinfo =item2.GetParameters();
+                    foreach (var p in pinfo)
+                    {
+                        Console.WriteLine("Parameter Name: " + p.Name);
+                        Console.WriteLine("Parameter Position: " + p.Position);
+                        Console.WriteLine("Parameter Type: " + p.ParameterType);
+                    }
+                }
+                 t = asm.GetType(item.FullName);
+                object obj =Activator.CreateInstance(t);
+               
+               
+            
+               double cube=(double)item.InvokeMember("Cube1",BindingFlags.Public |BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj,new object[1]{400.0d});
+                Console.WriteLine("Cube number is" + " " +cube);
+              double sqrt=(double) item.InvokeMember("squareroot",BindingFlags.Public | BindingFlags.Instance | BindingFlags.InvokeMethod, null, obj, new object[1]{200.0d});
+                Console.WriteLine("Square root number is" + " " + sqrt); 
+                break;
+            }
+            Console.ReadLine();
+           
+
+        }
+    }
+}
